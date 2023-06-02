e4c26bd06f005fdfbb19a8963ceaf2854ba01316
felixwhite <klassemax@gmail.com>
2023-06-02 17:39:45 +0800

    bloom filter now initializes properly

--- bloom.py
+++ bloom.py
@@ -5,13 +5,14 @@ class BloomFilter:
     def __init__(self, size : int, num_hash_functions: int):
         self.size = size
         self.bitarray = bitarray(size)
+        self.bitarray.setall(0)
         self.num_hash_functions = num_hash_functions
 
-    def insert(self, element):
+    def insert(self, element : str) -> None:
         for i in range(self.num_hash_functions):
             self.bitarray[mmh3.hash(key=element, seed=i) % self.size] = 1
 
-    def lookup(self, element):
+    def lookup(self, element : str) -> bool:
         for i in range(self.num_hash_functions):
             if self.bitarray[mmh3.hash(key=element, seed=i) % self.size] == 0:
                 return False
---
+++ main.py
@@ -0,0 +1,11 @@
+from bloom import BloomFilter
+
+m = 1000 #bitarray length
+k = 12 #number of hash functions
+
+bf = BloomFilter(size=m, num_hash_functions=k)
+
+print(bf.lookup("HI"))
+bf.insert("HI")
+print(bf.lookup("HI"))
+
